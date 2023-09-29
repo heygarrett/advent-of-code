3dbc4e279460dce3278604a1897f09989f63dced
Garrett Oreilly <hey@heygarrett.com>
2023-09-28 21:44:34 -0400

    functioning API with solution for day1 problem 1

---
+++ day1/day1.go
@@ -0,0 +1,57 @@
+package day1
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func GetMaxCalories(input string) (int, error) {
+	parsedInput := parseInput(input)
+	elfSums := make([]int, 0)
+	for _, elf := range parsedInput {
+		intList, err := convertStrListToIntList(elf)
+		if err != nil {
+			return 0, err
+		}
+		intTotal := sumAll(intList)
+		elfSums = append(elfSums, intTotal)
+	}
+
+	return slices.Max(elfSums), nil
+}
+
+func parseInput(input string) [][]string {
+	splitOnBlankLines := strings.Split(input, "\n\n")
+	splitOnNewLines := make([][]string, 0)
+	for _, contiguousLines := range splitOnBlankLines {
+		splitOnNewLines = append(
+			splitOnNewLines,
+			strings.Split(contiguousLines, "\n"),
+		)
+	}
+
+	return splitOnNewLines
+}
+
+func sumAll(numbers []int) int {
+	sum := 0
+	for _, n := range numbers {
+		sum += n
+	}
+
+	return sum
+}
+
+func convertStrListToIntList(strs []string) ([]int, error) {
+	result := make([]int, 0)
+	for _, s := range strs {
+		converted, err := strconv.ParseInt(s, 0, 0)
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, int(converted))
+	}
+
+	return result, nil
+}
---
+++ day1/day1_test.go
@@ -0,0 +1,65 @@
+package day1_test
+
+import (
+	"2022/day1"
+	"strings"
+	"testing"
+)
+
+func TestGetMaxCalories(t *testing.T) {
+	t.Run("Test 1", func(t *testing.T) {
+		calories := strings.Join([]string{"1000", "2000", "", "3000", "4000"}, "\n")
+		got, err := day1.GetMaxCalories(calories)
+		if err != nil {
+			t.Error(err)
+		}
+
+		want := 7000
+
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
+
+	t.Run("Test 2", func(t *testing.T) {
+		calories := strings.Join([]string{"10000", "2000", "", "3000", "4000"}, "\n")
+		got, err := day1.GetMaxCalories(calories)
+		if err != nil {
+			t.Error(err)
+		}
+
+		want := 12000
+
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
+	t.Run("Example input", func(t *testing.T) {
+		calories := strings.Join([]string{
+			"1000",
+			"2000",
+			"3000",
+			"",
+			"4000",
+			"",
+			"5000",
+			"6000",
+			"",
+			"7000",
+			"8000",
+			"9000",
+			"",
+			"10000",
+		}, "\n")
+		got, err := day1.GetMaxCalories(calories)
+		if err != nil {
+			t.Error(err)
+		}
+
+		want := 24000
+
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
+}
