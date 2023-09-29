5e74e11988ad85bac8f9225b7d87adfc055fb59d
Garrett Oreilly <hey@heygarrett.com>
2023-09-28 21:42:47 -0400

    add reader package for files

---
+++ reader.go
@@ -0,0 +1,27 @@
+package reader
+
+import (
+	"bufio"
+	"io"
+	"io/fs"
+	"strings"
+)
+
+func NewInputFromFS(fileSystem fs.FS, fileName string) (string, error) {
+	file, err := fileSystem.Open(fileName)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	return readFile(file), nil
+}
+
+func readFile(file io.Reader) string {
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return strings.Join(lines, "\n")
+}
---
+++ reader_test.go
@@ -0,0 +1,40 @@
+package reader_test
+
+import (
+	reader "2022"
+	"testing"
+	"testing/fstest"
+)
+
+func TestReadFile(t *testing.T) {
+	const (
+		example1 = "hi there hello"
+		example2 = "now you're gtting it"
+	)
+	fs := fstest.MapFS{
+		"example1.txt": {Data: []byte(example1)},
+		"example2.txt": {Data: []byte(example2)},
+	}
+
+	t.Run("example1", func(t *testing.T) {
+		input, err := reader.NewInputFromFS(fs, "example1.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if input != example1 {
+			t.Errorf("got %s, wanted %s", input, example1)
+		}
+	})
+
+	t.Run("example2", func(t *testing.T) {
+		input, err := reader.NewInputFromFS(fs, "example2.txt")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if input != example2 {
+			t.Errorf("got %s, wanted %s", input, example2)
+		}
+	})
+}
