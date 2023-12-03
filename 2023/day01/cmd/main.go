dccbd1bd99924ef332beb9f18d83ae61c9856a52
Garrett Oreilly <hey@heygarrett.com>
2023-12-02 20:10:31 -0500

    feat(2023/day01): add main package

---
+++ 2023/day01/cmd/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	solutions "2023/day01"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+)
+
+func main() {
+	rawInput, err := fs.ReadFile(os.DirFS("day01"), "input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	input := string(rawInput)
+
+	part1 := solutions.Part1(input)
+	fmt.Printf("Part 1: %d\n", part1)
+}
