2e018beebf6fae5d589a9acfe3b323ddabd3c3b3
Garrett Oreilly <hey@heygarrett.com>
2023-12-02 19:18:06 -0500

    test(2023/day01): more failing tests for part 1

--- 2023/day01/solutions_test.go
+++ 2023/day01/solutions_test.go
@@ -2,23 +2,47 @@ package solutions_test
 
 import (
 	solutions "2023/day01"
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
 	"testing"
 )
 
+var testCases = []struct {
+	input  string
+	output int
+}{
+	{input: "1abc2", output: 12},
+	{input: "pqr3stu8vwx", output: 38},
+	{input: "a1b2c3d4e5f", output: 15},
+	{input: "treb7uchet", output: 77},
+}
+
 func TestPart1(t *testing.T) {
-	rawInput, err := fs.ReadFile(os.DirFS("."), "example.txt")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	input := string(rawInput)
+	t.Run("example.txt", func(t *testing.T) {
+		rawInput, err := fs.ReadFile(os.DirFS("."), "example.txt")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input := string(rawInput)
 
-	got := solutions.Part1(input)
-	want := 142
+		got := solutions.Part1(input)
+		want := 142
 
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
+
+	for index, tc := range testCases {
+		t.Run(fmt.Sprintf("Test case: %d", index+1), func(t *testing.T) {
+			got := solutions.Part1(tc.input)
+			want := tc.output
+
+			if got != want {
+				t.Errorf("got %d, wanted %d", got, want)
+			}
+		})
 	}
 }
