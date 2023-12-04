c55466b492893f9242dc08a1028765c3031e1831
Garrett Oreilly <hey@heygarrett.com>
2023-12-04 17:10:15 -0500

    test(2023/day02): add more tests for part 1

--- 2023/day02/solutions_test.go
+++ 2023/day02/solutions_test.go
@@ -2,6 +2,7 @@ package solutions_test
 
 import (
 	solutions "2023/day02"
+	"fmt"
 	"io/fs"
 	"log"
 	"os"
@@ -15,10 +16,48 @@ func TestPart1(t *testing.T) {
 	}
 	input := string(rawInput)
 
-	got := solutions.Part1(input)
-	want := 8
+	t.Run("example.txt", func(t *testing.T) {
+		got := solutions.Part1(input)
+		want := 8
 
-	if got != want {
-		t.Errorf("got %d, wanted %d", got, want)
+		if got != want {
+			t.Errorf("got %d, wanted %d", got, want)
+		}
+	})
+	testCases := []struct {
+		input  string
+		output int
+	}{
+		{
+			input:  "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			output: 1,
+		},
+		{
+			input:  "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			output: 2,
+		},
+		{
+			input:  "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			output: 0,
+		},
+		{
+			input:  "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+			output: 0,
+		},
+		{
+			input:  "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+			output: 5,
+		},
+	}
+
+	for index, tc := range testCases {
+		t.Run(fmt.Sprintf("Test case: %d", index+1), func(t *testing.T) {
+			got := solutions.Part1(tc.input)
+			want := tc.output
+
+			if got != want {
+				t.Errorf("got %d, wanted %d", got, want)
+			}
+		})
 	}
 }
