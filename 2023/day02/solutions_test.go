69c40361c57fd1e09a2e71c38c4c9f8d01b5ead0
Garrett Oreilly <hey@heygarrett.com>
2023-12-07 14:21:32 -0500

    refactor(2023/day02): move test cases out of part 1 test function

--- 2023/day02/solutions_test.go
+++ 2023/day02/solutions_test.go
@@ -19,6 +19,34 @@ func getExampleInput() string {
 	return input
 }
 
+type output struct{ part1 int }
+
+var testCases = []struct {
+	input  string
+	output output
+}{
+	{
+		input:  "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+		output: output{part1: 1},
+	},
+	{
+		input:  "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+		output: output{part1: 2},
+	},
+	{
+		input:  "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+		output: output{part1: 0},
+	},
+	{
+		input:  "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+		output: output{part1: 0},
+	},
+	{
+		input:  "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+		output: output{part1: 5},
+	},
+}
+
 func TestPart1(t *testing.T) {
 	t.Run("example.txt", func(t *testing.T) {
 		got := solutions.Part1(getExampleInput())
@@ -28,36 +56,11 @@ func TestPart1(t *testing.T) {
 			t.Errorf("got %d, wanted %d", got, want)
 		}
 	})
-	testCases := []struct {
-		input  string
-		output int
-	}{
-		{
-			input:  "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-			output: 1,
-		},
-		{
-			input:  "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-			output: 2,
-		},
-		{
-			input:  "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-			output: 0,
-		},
-		{
-			input:  "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-			output: 0,
-		},
-		{
-			input:  "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
-			output: 5,
-		},
-	}
 
 	for index, tc := range testCases {
 		t.Run(fmt.Sprintf("Test case: %d", index+1), func(t *testing.T) {
 			got := solutions.Part1(tc.input)
-			want := tc.output
+			want := tc.output.part1
 
 			if got != want {
 				t.Errorf("got %d, wanted %d", got, want)
