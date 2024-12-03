eed1b2f233b802440642fcf2f14668cb3750b85c
Garrett <hey@heygarrett.com>
2024-12-03 01:36:27 -0500

    feat(2024-day1): part 2 solution

--- 2024/src/bin/day1/main.rs
+++ 2024/src/bin/day1/main.rs
@@ -2,12 +2,37 @@ use std::{fs, iter::zip};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let contents = fs::read_to_string("src/bin/day1/input.txt")?;
-	let answer = part1(contents);
-	println!("{}", answer);
+
+	let answer = part1(&contents);
+	println!("part 1: {}", answer);
+
+	let answer = part2(&contents);
+	println!("part 2: {}", answer);
+
 	Ok(())
 }
 
-fn part1(input: String) -> u32 {
+fn part1(input: &str) -> u32 {
+	let (mut list1, mut list2) = generate_lists(input);
+
+	list1.sort();
+	list2.sort();
+
+	zip(list1, list2)
+		.map(|pair| u32::abs_diff(pair.0, pair.1))
+		.sum()
+}
+
+fn part2(input: &str) -> u32 {
+	let (list1, list2) = generate_lists(input);
+
+	list1
+		.iter()
+		.map(|num1| num1 * list2.iter().filter(|&num2| num1 == num2).count() as u32)
+		.sum()
+}
+
+fn generate_lists(input: &str) -> (Vec<u32>, Vec<u32>) {
 	let mut list1: Vec<u32> = vec![];
 	let mut list2: Vec<u32> = vec![];
 
@@ -21,12 +46,7 @@ fn part1(input: String) -> u32 {
 		list2.push(numbers[1]);
 	});
 
-	list1.sort();
-	list2.sort();
-
-	zip(list1, list2)
-		.map(|pair| u32::abs_diff(pair.0, pair.1))
-		.sum()
+	(list1, list2)
 }
 
 #[cfg(test)]
@@ -37,6 +57,13 @@ mod tests {
 	fn test_part1() {
 		let contents =
 			fs::read_to_string("src/bin/day1/example.txt").expect("expecting example.txt");
-		assert_eq!(part1(contents), 11);
+		assert_eq!(part1(&contents), 11);
+	}
+
+	#[test]
+	fn test_part2() {
+		let contents =
+			fs::read_to_string("src/bin/day1/example.txt").expect("expecting example.txt");
+		assert_eq!(part2(&contents), 31);
 	}
 }
