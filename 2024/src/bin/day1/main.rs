600345eb3a4c0e0cfa3f58c7d209e4169aef8cd2
Garrett <hey@heygarrett.com>
2024-12-02 21:19:54 -0500

    refactor(2024-day1): refactor part 1 solution
    feat(2024-day1): part 2 solution
    
    feat(2024-day1): part 2 solution

--- 2024/src/bin/day1/main.rs
+++ 2024/src/bin/day1/main.rs
@@ -2,24 +2,30 @@ use std::{fs, iter::zip};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let contents = fs::read_to_string("src/bin/day1/input.txt")?;
-	let answer = calculate_distance(contents);
+	let answer = part1(contents);
 	println!("{}", answer);
 	Ok(())
 }
 
-fn calculate_distance(input: String) -> u32 {
+fn part1(input: String) -> u32 {
 	let mut list1: Vec<u32> = vec![];
 	let mut list2: Vec<u32> = vec![];
-	for line in input.lines() {
-		let mut nums = line.split_whitespace();
-		list1.push(nums.next().expect("number").parse::<u32>().expect("number"));
-		list2.push(nums.next().expect("number").parse::<u32>().expect("number"));
-	}
+
+	input.lines().for_each(|line| {
+		let numbers: Vec<u32> = line
+			.split_whitespace()
+			.map(|num_str| num_str.parse().expect("should be a number"))
+			.collect();
+
+		list1.push(numbers[0]);
+		list2.push(numbers[1]);
+	});
+
 	list1.sort();
 	list2.sort();
 
 	zip(list1, list2)
-		.map(|tup| u32::abs_diff(tup.0, tup.1))
+		.map(|pair| u32::abs_diff(pair.0, pair.1))
 		.sum()
 }
 
@@ -28,9 +34,9 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn test_calculate_distance() {
+	fn test_part1() {
 		let contents =
 			fs::read_to_string("src/bin/day1/example.txt").expect("expecting example.txt");
-		assert_eq!(calculate_distance(contents), 11);
+		assert_eq!(part1(contents), 11);
 	}
 }
