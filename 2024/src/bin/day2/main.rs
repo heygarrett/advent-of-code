95ffcec05bc98781ca3ae047bba356583d9393f9
Garrett <hey@heygarrett.com>
2024-12-04 21:06:21 -0500

    feat(2024-day2): part 2 solution

--- 2024/src/bin/day2/main.rs
+++ 2024/src/bin/day2/main.rs
@@ -2,31 +2,61 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let input = fs::read_to_string("src/bin/day2/input.txt")?;
+	let parsed_input = parse_input(input);
 
-	let answer = part1(input);
+	let answer = part1(&parsed_input);
 	println!("part 1: {}", answer);
+
+	let answer = part2(&parsed_input);
+	println!("part 2: {}", answer);
 
 	Ok(())
 }
 
-fn part1(input: String) -> usize {
-	input
-		.lines()
+fn part1(lines: &[Vec<isize>]) -> usize {
+	lines.iter().filter(|&line| get_safety(line)).count()
+}
+
+fn part2(lines: &[Vec<isize>]) -> usize {
+	lines
+		.iter()
 		.filter(|&line| {
-			let numbers: Vec<isize> = line
-				.split_whitespace()
-				.map(|num_str| num_str.parse().expect("should be a number"))
-				.collect();
+			if get_safety(line) {
+				return true;
+			}
 
-			let ascending = numbers.windows(2).all(|w| w[0] < w[1]);
-			let descending = numbers.windows(2).all(|w| w[0] > w[1]);
-			let valid_level_diff = numbers
-				.windows(2)
-				.all(|w| matches!((w[0] - w[1]).abs_diff(0), 1..=3));
+			for index in 0..line.len() {
+				let mut line_subset = line.to_vec();
+				line_subset.remove(index);
+				if get_safety(&line_subset) {
+					return true;
+				}
+			}
 
-			(ascending || descending) && valid_level_diff
+			false
 		})
 		.count()
+}
+
+fn parse_input(input: String) -> Vec<Vec<isize>> {
+	input
+		.lines()
+		.map(|line| {
+			line.split_whitespace()
+				.map(|num_str| num_str.parse().expect("should be a number"))
+				.collect()
+		})
+		.collect()
+}
+
+fn get_safety(numbers: &[isize]) -> bool {
+	let ascending = numbers.windows(2).all(|w| w[0] < w[1]);
+	let descending = numbers.windows(2).all(|w| w[0] > w[1]);
+	let valid_level_diff = numbers
+		.windows(2)
+		.all(|w| matches!((w[0] - w[1]).abs_diff(0), 1..=3));
+
+	(ascending || descending) && valid_level_diff
 }
 
 #[cfg(test)]
@@ -39,6 +69,11 @@ mod tests {
 
 	#[test]
 	fn test_part1() {
-		assert_eq!(part1(get_example_input()), 2);
+		assert_eq!(part1(&parse_input(get_example_input())), 2);
+	}
+
+	#[test]
+	fn test_part2() {
+		assert_eq!(part2(&parse_input(get_example_input())), 4);
 	}
 }
