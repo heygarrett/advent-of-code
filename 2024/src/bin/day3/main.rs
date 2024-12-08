a224a55a7b13b6677d7416c9bca645729c81cbc3
Garrett <hey@heygarrett.com>
2024-12-08 10:50:29 -0500

    feat(2024-day3): part 2 solution

--- 2024/src/bin/day3/main.rs
+++ 2024/src/bin/day3/main.rs
@@ -3,18 +3,34 @@ use std::fs;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let input = fs::read_to_string("src/bin/day3/input.txt")?;
 
-	let answer = part1(input);
+	let answer = part1(&input);
 	println!("part 1: {}", answer);
+
+	let answer = part2(&input);
+	println!("part 2: {}", answer);
 
 	Ok(())
 }
 
-fn part1(input: String) -> usize {
+fn part1(input: &str) -> usize {
+	parse_and_calculate(input)
+}
+
+fn part2(input: &str) -> usize {
+	let mut sum = 0;
+	for split in input.split("do").filter(|&s| &s[..5] != "n't()") {
+		sum += parse_and_calculate(split);
+	}
+
+	sum
+}
+
+fn parse_and_calculate(input: &str) -> usize {
 	input
 		.split("mul")
-		.filter(|&s| s.chars().next().is_some_and(|c| c == '('))
-		.filter_map(|s| s[1..].splitn(2, ")").take(1).next().take())
-		.filter_map(|s| s.split_once(",").take())
+		.filter(|&s| &s[..1] == "(")
+		.filter_map(|s| s[1..].splitn(2, ")").take(1).next())
+		.filter_map(|s| s.split_once(","))
 		.filter_map(|(x, y)| {
 			x.parse::<usize>().ok().and_then(|parsed_x| {
 				y.parse::<usize>().ok().map(|parsed_y| parsed_x * parsed_y)
@@ -27,12 +43,17 @@ fn part1(input: String) -> usize {
 mod tests {
 	use super::*;
 
-	fn get_example_input() -> String {
-		fs::read_to_string("src/bin/day3/example.txt").expect("should be example input")
+	#[test]
+	fn test_part1() {
+		let example_input = fs::read_to_string("src/bin/day3/example.txt")
+			.expect("should be example input");
+		assert_eq!(part1(&example_input), 161);
 	}
 
 	#[test]
-	fn test_part1() {
-		assert_eq!(part1(get_example_input()), 161);
+	fn test_part2() {
+		let example_input = fs::read_to_string("src/bin/day3/example2.txt")
+			.expect("should be example input");
+		assert_eq!(part2(&example_input), 48);
 	}
 }
