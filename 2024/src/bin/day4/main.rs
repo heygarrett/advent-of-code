a512a81295b4fd75c5a28a70eedbe18413af0a34
Garrett <hey@heygarrett.com>
2024-12-14 13:58:44 -0500

    feat(2024-day4): part 2 solution

--- 2024/src/bin/day4/main.rs
+++ 2024/src/bin/day4/main.rs
@@ -3,13 +3,16 @@ use std::fs;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let input = fs::read_to_string("src/bin/day4/input.txt")?;
 
-	let answer = part1(input);
+	let answer = part1(&input);
 	println!("part 1: {}", answer);
+
+	let answer = part2(&input);
+	println!("part 2: {}", answer);
 
 	Ok(())
 }
 
-fn part1(input: String) -> u32 {
+fn part1(input: &str) -> u32 {
 	input
 		.lines()
 		.enumerate()
@@ -46,13 +49,53 @@ fn part1(input: String) -> u32 {
 
 								Some(letter)
 									== input.lines().nth(coordinates.1 as usize).and_then(
-										|nth_line| nth_line.chars().nth(coordinates.0 as usize),
+										|nth_line| {
+											nth_line.chars().nth(coordinates.0 as usize)
+										},
 									)
 							})
 						})
 						.count() as u32
 				})
 				.sum::<u32>()
+		})
+		.sum()
+}
+
+fn part2(input: &str) -> u32 {
+	input
+		.lines()
+		.enumerate()
+		.map(|(line_index, line)| {
+			line.chars()
+				.enumerate()
+				.filter(|(ch_index, ch)| {
+					if *ch != 'A' {
+						return false;
+					}
+					let directions: [(i32, i32); 4] =
+						[(1, -1), (1, 1), (-1, 1), (-1, -1)];
+
+					["MMSS", "SMMS", "SSMM", "MSSM"]
+						.iter()
+						.any(|&valid_orientation| {
+							Some(valid_orientation.to_string())
+								== directions
+									.iter()
+									.map(|(x, y)| {
+										let coordinates =
+											(*ch_index as i32 + x, line_index as i32 + y);
+										input
+											.lines()
+											.nth(coordinates.1 as usize)
+											.and_then(|line| {
+												line.chars().nth(coordinates.0 as usize)
+											})
+									})
+									.collect::<Option<String>>()
+						})
+				})
+				.count() as u32
 		})
 		.sum()
 }
@@ -68,6 +111,12 @@ mod tests {
 	#[test]
 	fn test_part1() {
 		let example_input = get_example_input();
-		assert_eq!(part1(example_input), 18);
+		assert_eq!(part1(&example_input), 18);
+	}
+
+	#[test]
+	fn test_part2() {
+		let example_input = get_example_input();
+		assert_eq!(part2(&example_input), 9);
 	}
 }
